b178ba0229d9b1d56360d1e0fc38dc1d47bcc79e
Your Name <ajanebenedict@gmail.com>
2021-08-24 16:21:52 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+import MakeEasy from "./homepage/makeEasy";
+import ForYou from "./homepage/forYou";
+import Interact from "./homepage/interact";
+
+import SliderCarousel from "./homepage/carousel";
+import Contact from "./homepage/Contact";
+const easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeIn = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      ease: easing,
+    },
+  },
+};
+
+export default function Home() {
+  return (
+    <motion.div
+    // initial="initial"
+    // animate="animate"
+    // exit={{ opacity: 0 }}
+    // variants={fadeIn}
+    >
+      <div style={{ maxWidth: "1440px", margin: "auto" }}>
+        <MakeEasy />
+        <ForYou />
+        <Interact />
+        
+        <SliderCarousel />
+
+       
+        <Contact />
+      </div>
+    </motion.div>
+  );
+}
---
+++ printname.py
@@ -0,0 +1,2 @@
+name="Ajane Benedict Lishileyemse"
+print(name)
